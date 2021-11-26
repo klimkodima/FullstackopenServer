2e4eda99973776c4e33c87c05cfa7ff6e6ebea1f
klimkodima <klimkodima@tut.by>
2021-11-26 14:18:20 +0300

    part3.8

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+     JSON.stringify(req.body)
+    ].join(' ')
+  }))
 app.use(express.json())
 
 let people = [
